a0130437edb4ebb614f386ff4a2553044b2f34f1
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-09-20 18:10:22 +1000

    Add a really basic about me page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,19 @@
+import { roboto_mono } from '../fonts'
+
 export default function About() {
-  return <main className='w-full max-w-screen-lg'>About</main>
+  return (
+    <main className='mb-16 w-full max-w-screen-lg px-6 sm:my-16 md:px-10'>
+      <section className='mb-16 text-sm text-gray-700 dark:text-gray-300'>
+        <h2 className={`${roboto_mono.className} mb-2 text-xl text-black dark:text-white`}>
+          About Me
+        </h2>
+        <p className='mb-2'>
+          My name is Matthew Hiebl. I graduate my computer science degree from UNSW in December
+          2023.
+        </p>
+        <p className='mb-2'>{"There isn't too much to say other than that..."}</p>
+        <p className='mb-2'>Check out my socials in the footer!</p>
+      </section>
+    </main>
+  )
 }

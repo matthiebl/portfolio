24bde50ba29d6ad20c6870796c685b5effbc34f1
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-09-20 17:26:07 +1000

    Add footer to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { roboto } from './fonts'
 import { ThemeProvider } from './theme'
 import { Navbar } from '@/components/Navbar'
+import { Footer } from '@/components/Footer'
 
 export const metadata: Metadata = {
   title: 'matthiebl | Home',
@@ -12,10 +13,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={roboto.className + ' dark:bg-slate-950'}>
+      <body className={roboto.className + ' flex flex-col dark:bg-slate-950'}>
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
           <Navbar />
           <div className='flex w-full justify-center'>{children}</div>
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,9 +1,32 @@
+import { BiLogoGithub, BiLogoLinkedinSquare } from 'react-icons/bi'
+import { HiArrowUpRight } from 'react-icons/hi2'
+
 import { roboto_mono } from '@/app/fonts'
 
 export function Footer({}: {}) {
   return (
-    <footer className={`${roboto_mono.className} flex w-full justify-center`}>
-      <div className='flex w-full max-w-screen-xl items-center justify-center gap-4'></div>
+    <footer className={`${roboto_mono.className} mt-auto flex w-full justify-center`}>
+      <div className='flex w-full max-w-screen-xl flex-wrap items-center justify-center gap-5 p-3 text-gray-500 sm:text-sm'>
+        <span className='text-sm'>Matthew Hiebl 2023</span>
+        <a
+          href='https://github.com/matthiebl'
+          target='_blank'
+          className='flex items-center gap-1 rounded-full p-2 hover:underline'
+        >
+          <BiLogoGithub />
+          <span className='hidden sm:block'>Github</span>
+          <HiArrowUpRight className='hidden sm:block' />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/matthew-hiebl-330544266/'
+          target='_blank'
+          className='flex items-center gap-1 rounded-full p-2 hover:underline'
+        >
+          <BiLogoLinkedinSquare />
+          <span className='hidden sm:block'>LinkedIn</span>
+          <HiArrowUpRight className='hidden sm:block' />
+        </a>
+      </div>
     </footer>
   )
 }
